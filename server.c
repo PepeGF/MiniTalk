21bbf46a651011da15ac77864310d8aa2359b153
José García <gran.pepe@gmail.com>
2021-12-12 21:12:03 +0100

    Arreglar LEN

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 16:32:40 by josgarci          #+#    #+#             */
-/*   Updated: 2021/12/12 20:08:27 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/12/12 20:47:51 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*g_str;
 
-static void	ft_print_pid(void)
+/*static void	ft_print_pid(void)
 {
 	write (1, "PID: ", 5);
 	ft_putnbr_fd(getpid(), 1);
 	write (1, "\n", 1);
 	return ;
 }
-
+*/
 static void	ft_sig_handler_len(int sig, siginfo_t *info, void *ucontext)
 {
 	static int	i = 1;
 	static int	j;
-	static int	c;
+	static unsigned int	c;
 
 	(void)ucontext;
 	if (sig == SIGUSR1)
@@ -58,7 +58,7 @@ static int	ft_receive_len(void)
 
 	i = 0;
 	len_str = 0;
-	g_str = malloc(sizeof(int));
+	g_str = calloc(/*sizeof(int)*/1, 4);
 	if (!g_str)
 		exit(EXIT_FAILURE);
 	sa_len.sa_flags = SA_SIGINFO;
@@ -69,6 +69,9 @@ static int	ft_receive_len(void)
 		pause();
 	len_str = (int)g_str[0];
 	free(g_str);
+	write(1, "\n------\n| ", 10);
+	ft_putnbr_fd(len_str, 1);
+	write(1, " |\n------\n", 10);
 	return (len_str);
 }
 
@@ -105,16 +108,22 @@ static void	ft_receive_str(int len_str)
 	int					i;
 	struct sigaction	sa_char;
 
-	g_str = malloc(sizeof(char) * (len_str + 1));
+	g_str = calloc(sizeof(char), (len_str + 1));
 	if (!g_str)
+	{
+//		write (1, "Tu calloc apesta\n", 17);
 		exit(EXIT_FAILURE);
+	}
+	//g_str[len_str] = '\0';
 	sa_char.sa_flags = SA_SIGINFO;
 	sa_char.sa_sigaction = &ft_sig_handler_str;
 	sigaction(SIGUSR1, &sa_char, NULL);
 	sigaction(SIGUSR2, &sa_char, NULL);
 	i = 0;
 	while (++i <= (len_str + 1) * 8)
+	{
 		pause();
+	}
 	return ;
 }
 
@@ -122,7 +131,11 @@ int	main(void)
 {
 	int	len_str;
 
-	ft_print_pid();
+	write (1, "PID: ", 5);
+	ft_putnbr_fd(getpid(), 1);
+	write (1, "\n", 1);
+
+//	ft_print_pid();
 	while (1)
 	{
 		len_str = ft_receive_len();

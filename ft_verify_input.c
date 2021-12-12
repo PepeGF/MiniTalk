92e0c757d31fb961dd6b6d1d3fb2ffb4a2c732bf
José García <gran.pepe@gmail.com>
2021-12-12 22:31:17 +0100

    Verificado

--- ft_verify_input.c
+++ ft_verify_input.c
@@ -6,7 +6,7 @@
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 22:08:17 by josgarci          #+#    #+#             */
-/*   Updated: 2021/12/12 22:10:15 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/12/12 22:28:08 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_verify_input(int argc, char **argv)
 		exit(1);
 	}
 	pid_server = ft_atoi(argv[1]);
-	if (pid_server < 0)
+	if (pid_server <= 0)
 	{
 		write(1, "PID del servidor incorrecto.\n", 29);
 		exit (1);
